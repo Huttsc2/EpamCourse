d6a90eabca736b0034964dda113af6bbf1b59b14
Mike <sicsid1@gmail.com>
2023-07-21 19:36:40 +0400

    merge from DevelopmentAndBuildTools

---
+++ EpamCourse/Basic of .NET Framework and C#/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamCourse.Basic_of_.NET_Framework_and_C_
+{
+    public class Class1
+    {
+
+    }
+}
